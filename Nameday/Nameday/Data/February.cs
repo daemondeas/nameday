f04bd425069a07432113eb715b5b0553b253de42
Andreas Svanström <andreassvanstrom@andreass-imac.mynet>
2020-11-22 22:38:01 +0100

    Add implementations of February and March

---
+++ Nameday/Nameday/Data/February.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Nameday.Data
+{
+    public class February : Month
+    {
+        public February()
+        {
+        }
+
+        public override int NumberOfDays => 29;
+
+        public override string Name => "februari";
+
+        protected override string[][] Names => new[] {
+				new [] { "Max", "Maximilian" },
+ 				Array.Empty<string>(),
+				new [] { "Disa", "Hjördis" },
+				new [] { "Ansgar", "Anselm" },
+				new [] { "Agata", "Agda" },
+				new [] { "Dorotea", "Doris" },
+				new [] { "Rikard", "Dick" },
+				new [] { "Berta", "Bert" },
+				new [] { "Fanny", "Franciska" },
+				new [] { "Iris" },
+				new [] { "Yngve", "Inge" },
+				new [] { "Evelina", "Evy" },
+				new [] { "Agne", "Ove" },
+				new [] { "Valentin" },
+				new [] { "Sigfrid" },
+				new [] { "Julia", "Julius" },
+				new [] { "Alexandra", "Sandra" },
+				new [] { "Frida", "Fritiof" },
+				new [] { "Gabriella", "Ella" },
+				new [] { "Vivianne" },
+				new [] { "Hilding" },
+				new [] { "Pia" },
+				new [] { "Torsten", "Torun" },
+				new [] { "Mattias", "Mats" },
+				new [] { "Sigvard", "Sivert" },
+				new [] { "Torgny", "Torkel" },
+				new [] { "Lage" },
+				new [] { "Maria" },
+ 				Array.Empty<string>()
+		};
+    }
+}
---
+++ Nameday/Nameday/Data/March.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Nameday.Data
+{
+    public class March : Month
+    {
+        public March()
+        {
+        }
+
+        public override int NumberOfDays => 31;
+
+        public override string Name => "mars";
+
+        protected override string[][] Names => new[] {
+				new [] { "Albin", "Elvira" },
+				new [] { "Ernst", "Erna" },
+				new [] { "Gunborg", "Gunvor" },
+				new [] { "Adrian", "Adriana" },
+				new [] { "Tora", "Tove" },
+				new [] { "Ebba", "Ebbe" },
+				new [] { "Camilla" },
+				new [] { "Siv" },
+				new [] { "Torbjörn", "Torleif" },
+				new [] { "Edla", "Ada" },
+				new [] { "Edvin", "Egon" },
+				new [] { "Viktoria" },
+				new [] { "Greger" },
+				new [] { "Matilda", "Maud" },
+				new [] { "Kristoffer", "Christel" },
+				new [] { "Herbert", "Gilbert" },
+				new [] { "Gertrud" },
+				new [] { "Edvard", "Edmund" },
+				new [] { "Josef", "Josefina" },
+				new [] { "Joakim", "Kim" },
+				new [] { "Bengt" },
+				new [] { "Kennet", "Kent" },
+				new [] { "Gerda", "Gerd" },
+				new [] { "Gabriel", "Rafael" },
+ 				Array.Empty<string>(),
+				new [] { "Emanuel" },
+				new [] { "Rudolf", "Ralf" },
+				new [] { "Malkolm", "Morgan" },
+				new [] { "Jonas", "Jens" },
+				new [] { "Holger", "Holmfrid" },
+				new [] { "Ester" }
+		};
+    }
+}
